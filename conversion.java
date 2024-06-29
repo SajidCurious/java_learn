3b5c52b7f7af8c667a2fd825659092517196e308
Mohammed <sajidcurious9@gmail.com>
2024-06-29 22:35:58 +0530

    converted float to int using conversion technique

--- conversion.java
+++ conversion.java
@@ -1,7 +1,13 @@
 class conversion {
+    // public static void main(String args[]) {
+    // byte b = 127;
+    // int a = b;
+    // System.out.println(a);
+
+    // }
     public static void main(String args[]) {
-        byte b = 127;
-        int a = b;
+        float f = 2.5f;
+        int a = (int) f;
         System.out.println(a);
 
     }
