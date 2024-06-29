cd4ddf2b2acc45f5aacbd2894586e3f492680a99
Mohammed <sajidcurious9@gmail.com>
2024-06-29 22:39:43 +0530

    wrote simple code for the type promotion example

--- conversion.java
+++ conversion.java
@@ -5,10 +5,17 @@ class conversion {
     // System.out.println(a);
 
     // }
+    // public static void main(String args[]) {
+    // float f = 2.5f;
+    // int a = (int) f;
+    // System.out.println(a);
+
+    // }
     public static void main(String args[]) {
-        float f = 2.5f;
-        int a = (int) f;
-        System.out.println(a);
+        byte b = 10;
+        byte c = 20;
+        int r = b * c;
+        System.out.println(r);
 
     }
 }
