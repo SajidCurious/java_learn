1da0ba8a8b81688c8571d2aaf69378e8f371ad5c
Mohammed <sajidcurious9@gmail.com>
2024-06-29 23:17:48 +0530

    renamed

---
+++ relational.java
@@ -0,0 +1,42 @@
+class relational {
+    // public static void main(String[] args) {
+    // int x = 5;
+    // int y = 6;
+    // boolean r = x > y;
+    // System.out.println(r);
+    // }
+    // public static void main(String[] args) {
+    // int x = 5;
+    // int y = 6;
+    // boolean r = x < y;
+    // System.out.println(r);
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 6;
+    // int y = 6;
+    // boolean r = x >= y;
+    // System.out.println(r);
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 6;
+    // int y = 6;
+    // boolean r = x <= y;
+    // System.out.println(r);
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 6;
+    // int y = 6;
+    // boolean r = x != y;
+    // System.out.println(r);
+    // }
+
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 6;
+        boolean r = x == y;
+        System.out.println(r);
+    }
+}
