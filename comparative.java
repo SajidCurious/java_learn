f8068a36ec3057d0ca3701260b49cca63ebda2f2
Mohammed <sajidcurious9@gmail.com>
2024-06-29 23:12:49 +0530

    wrote siple code to test the comparative operators in java

---
+++ comparative.java
@@ -0,0 +1,42 @@
+public class comparative {
+    // public static void main(String[] args) {
+    // int x = 5;
+    // int y = 6;
+    // boolean r = x > y;
+    // System.out.println(r);
+    // }
+    // public static void main(String[] args) {
+    // int x = 5;
+    // int y = 6;
+    // boolean r = x < y;
+    // System.out.println(r);
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 6;
+    // int y = 6;
+    // boolean r = x >= y;
+    // System.out.println(r);
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 6;
+    // int y = 6;
+    // boolean r = x <= y;
+    // System.out.println(r);
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 6;
+    // int y = 6;
+    // boolean r = x != y;
+    // System.out.println(r);
+    // }
+
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 6;
+        boolean r = x == y;
+        System.out.println(r);
+    }
+}
