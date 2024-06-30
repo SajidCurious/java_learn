e36f39d598fc4d840f7ceec72642ffb03ab7cbd9
Mohammed <sajidcurious9@gmail.com>
2024-06-30 12:06:01 +0530

    wrote simple code to test logical operators in java

---
+++ logical.java
@@ -0,0 +1,44 @@
+class logical {
+    // public static void main(String[] args) {
+    // int x = 10;
+    // int y = 5;
+    // int a = 8;
+    // int b = 10;
+    // boolean r = x > y && a < b;
+    // System.out.println(r);
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 10;
+    // int y = 5;
+    // int a = 8;
+    // int b = 10;
+    // boolean r = x < y && a < b;
+    // System.out.println(r);
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 10;
+    // int y = 5;
+    // int a = 8;
+    // int b = 10;
+    // boolean r = x < y || a < b;
+    // System.out.println(r);
+    // }
+
+    // public static void main(String[] args) {
+    // int x = 10;
+    // int y = 5;
+    // int a = 8;
+    // int b = 10;
+    // boolean r = x > y && a < b;
+    // System.out.println(r);
+    // }
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        boolean r = x < y;
+        System.out.println(!r);
+    }
+}
