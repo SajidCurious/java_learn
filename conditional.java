04a621e39f958e07e1c5620fcbed4ef1a02f4753
Mohammed <sajidcurious9@gmail.com>
2024-06-30 13:09:23 +0530

    tested conditional statements, if else and ternary operators

---
+++ conditional.java
@@ -0,0 +1,62 @@
+class conditional {
+
+    // public static void main(String[] args) {
+
+    // int x = 10;
+    // int y = 5;
+    // if (x < y) {
+    // System.out.println("Hello World");
+    // } else {
+    // System.out.println("Sorry");
+    // }
+    // }
+
+    // public static void main(String[] args) {
+
+    // int x = 10;
+    // int y = 5;
+    // if (x > y) {
+    // System.out.println("Hello World");
+    // } else {
+    // System.out.println("Sorry");
+    // }
+    // }
+
+    // public static void main(String[] args) {
+
+    // int x = 10;
+    // int y = 5;
+    // if (x > y && x <= 10) {
+    // System.out.println("Hello World");
+    // } else {
+    // System.out.println("Sorry");
+    // }
+    // }
+
+    // public static void main(String[] args) {
+
+    // int x = 6;
+    // int y = 5;
+    // int z = 10;
+    // if (x > y && x > z) {
+    // System.out.println("Hello World");
+    // } else if (y > z) {
+    // System.out.println("Sorry");
+    // } else {
+    // System.out.println("Nothing works");
+
+    // }
+    // }
+
+    public static void main(String[] args) {
+
+        int x = 3;
+        int result = 0;
+
+        result = x % 2 == 0 ? 10 : 20;
+
+        System.out.println(result);
+
+    }
+
+}
