67ca61900989d55e45004879dca61516d3f082c4
Mohammed <sajidcurious9@gmail.com>
2024-07-01 13:21:32 +0530

    wrote while loop code in java

---
+++ whileLoop.java
@@ -0,0 +1,11 @@
+class whileLoop {
+    public static void main(String a[]) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Hi " + i);
+            i++;
+
+        }
+    }
+
+}
