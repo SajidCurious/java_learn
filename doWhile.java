c53e8ac6bdef2b592b84558b254651952d86de18
Mohammed <sajidcurious9@gmail.com>
2024-07-01 15:04:25 +0530

    did doWhile loop

---
+++ doWhile.java
@@ -0,0 +1,12 @@
+class doWhile {
+
+    public static void main(String a[]) {
+        int i = 0;
+        do {
+            System.out.println("Hello " + i);
+            i++;
+
+        } while (i <= 5);
+    }
+
+}
