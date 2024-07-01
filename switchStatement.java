51254f8a03f56dbe71921af6cb43c044c1885ffd
Mohammed <sajidcurious9@gmail.com>
2024-07-01 12:51:02 +0530

    used switch statement instead of if and else

---
+++ switchStatement.java
@@ -0,0 +1,30 @@
+class switchStatement {
+
+    public static void main(String a[]) {
+
+        int x = 5;
+        switch (x) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            default:
+                System.out.println("Please decide first");
+
+                break;
+        }
+    }
+
+}
